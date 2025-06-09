9db5ff38d11da523d0250621cd7201ee5085d8ee
Rutwik Balkhande <rutwikbalkhande42@gmail.com>
2025-06-09 23:41:11 +0530

    feature-third-branchchecking step to push code in third branch

---
+++ src/ThirdBranch.java
@@ -0,0 +1,13 @@
+public class ThirdBranch {
+
+    void thirdBranch()
+    {
+        System.out.println("github commnd to create new branch and push code on new branch git  ");
+
+        System.out.println("git checkout -b feature/third-branch");   // create new branch
+
+        System.out.println("git add . ");// add All File third branch
+
+        System.out.println("Push Command = git push origin features/third-Branch");
+    }
+}
