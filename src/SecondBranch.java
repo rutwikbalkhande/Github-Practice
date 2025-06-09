f3dd2a3cb5de3caf470ac74a62d22205c5c9de6f
Rutwik Balkhande <rutwikbalkhande42@gmail.com>
2025-06-09 23:25:51 +0530

    created new branch and write step to push in new branch

--- src/SecondBranch.java
+++ src/SecondBranch.java
@@ -1,13 +1,15 @@
 public class SecondBranch {
-    void seconf()
+    void secondBranch()
     {
         System.out.println("Added & merge second branch");
-        System.out.println("github commnd to create new branh and push code on new branch git  ");
+        System.out.println("github commnd to create new branch and push code on new branch git  ");
 
         System.out.println("git checkout -b feature/add-second-file\n");   // create new branch
 
         System.out.println("git add SecondBranch.txt");     // for add single file
 
-        System.out.println("git add . ");    // Sdd All FileSeco
+        System.out.println("git add . ");// Sdd All FileSeco
+
+        System.out.println("Push COmmand =git push origin features/add-Second-file");
     }
 }
