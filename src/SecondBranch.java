716da82172ab9507a5477e5d6039fce8d8a744f3
Rutwik Balkhande <rutwikbalkhande42@gmail.com>
2025-06-09 17:41:46 +0530

    added Second Branch

---
+++ src/SecondBranch.java
@@ -0,0 +1,13 @@
+public class SecondBranch {
+    void seconf()
+    {
+        System.out.println("Added & merge second branch");
+        System.out.println("github commnd to create new branh and push code on new branch git  ");
+
+        System.out.println("git checkout -b feature/add-second-file\n");   // create new branch
+
+        System.out.println("git add SecondBranch.txt");     // for add single file
+
+        System.out.println("git add . ");    // Sdd All FileSeco
+    }
+}
